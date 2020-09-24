dd60362dd06ac7ef13bbe96d090d7a5a8cf9f1f7
Relativiteit <alejocain@gmail.com>
2020-09-24 12:25:59 +0200

    Gebruiksinvoer tekst

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
+//program in in Dutch to show the lenght of input
 public class GebruikersInvoer {
+    public static void main(String[] args) {
+        //State welcome message
+        System.out.println("Voer een woord in");
+        //Read users input in java 11 var s = new Scanner(System.in); is also oke?
+        Scanner s = new Scanner(System.in);
+        //State variable text
+        var tekst = s.nextLine();
+        //Show lenght of users input
+        System.out.println(tekst.length());
+
+    }
 }
